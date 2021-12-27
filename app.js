3ea949ae7ca1517742b46ccf2e91edd961545faa
imonweb <contact@imonweb.co.uk>
2021-12-27 16:09:02 +0000

    part 7 completed

--- app.js
+++ app.js
@@ -21,3 +21,31 @@ overlay.addEventListener("click", () => {
   searchContainer.classList.add('hide');
   overlay.classList.remove('show');
 })
+
+ 
+/* ====== Mobile Version ====== */
+
+const menuIconContainer = document.querySelector("nav .menu");
+const navContainer = document.querySelector(".nav__container");
+
+menuIconContainer.addEventListener("click", () => {
+  navContainer.classList.toggle("active");
+})
+ 
+
+const searchBar = document.querySelector(".mobile__search-container .nav__search--bar");
+const nav = document.querySelector(".nav__container nav");
+const searchInput = document.querySelector(".mobile__search-container input");
+const cancelBtn = document.querySelector(".mobile__search-container .cancel-btn");
+
+searchInput.addEventListener("click", () => {
+  searchBar.classList.add('active');
+  nav.classList.add('move-up');
+  desktopNav.classList.add('move-down');
+})
+
+cancelBtn.addEventListener("click", () => {
+  searchBar.classList.remove('active');
+  nav.classList.remove('move-up');
+  desktopNav.classList.remove('move-down');
+})
